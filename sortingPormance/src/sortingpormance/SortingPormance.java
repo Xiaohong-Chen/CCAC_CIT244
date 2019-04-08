e1fddd6b5ad1529cead54dfde1d4c85afd461f32
Chen <xiaohong.chen@ccac.org>
2019-04-08 16:34:51 -0400

    timeSort

--- sortingPormance/src/sortingpormance/SortingPormance.java
+++ sortingPormance/src/sortingpormance/SortingPormance.java
@@ -8,10 +8,5 @@ public class SortingPormance {
 
     public static void main(String[] args) {
         Tester t = new Tester();
-        
-        
-        
-        
-
     }
 }
--- sortingPormance/src/sortingpormance/Tester.java
+++ sortingPormance/src/sortingpormance/Tester.java
@@ -1,6 +1,7 @@
 package sortingpormance;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -9,70 +10,90 @@ import java.util.Random;
 public class Tester {
 
     Tester() {
+        
+        System.out.println("Input the Array Size you want to test: ");
+        Scanner userInput = new Scanner(System.in);
+        int arrSize = userInput.nextInt();
+        
+        int[] arr = getRanArr(arrSize);
 
         Sorter b = new BubbleSorter();
         Sorter m = new MergeSorter();
-        //int[] bubSortSampleArr = {4, 6, 7, 3, 4, 6, 7, 8,};
-        //int[] merSortSampleArr = {4, 6, 7, 3, 4, 6, 7, 8,};
-        
-        int[] arr = getRanArr(30000);
-        int[] bubSortSampleArr = arr;
-        int[] merSortSampleArr = arr;
 
-        
-        int[] afterBubSort = b.sort(bubSortSampleArr);
-        System.out.println("BUBBLE SORT:");
-        for (int n = 0; n < afterBubSort.length; n++) {
-            System.out.print(afterBubSort[n] + ", ");
-        }
+        int[] merArrForRunTimes = arr.clone();
+        int[] bubArrForRunTimes = arr.clone();
 
+        //int[] arrForBubSort = arr.clone();
+        //int[] arrForMerSort = arr.clone();
+
+        //testSort("Bubble Sort", b, arrForBubSort);
+
+        //testSort("Merge Sort", m, arrForMerSort);
+        
+        long bt = displayRunTimes(b, bubArrForRunTimes);
+        long mt = displayRunTimes(m, merArrForRunTimes);
+        
         System.out.println("");
+        System.out.println("Test case description:              " + arrSize + " value aray with random int values");
+        System.out.println("Time performance for bubble sort:   " + bt);
+        System.out.println("Time performance for merge  sort:   " + mt);
+        System.out.println("Describe the results of the test:   " );
 
-        int[] afterMerSort = m.sort(merSortSampleArr);
-        System.out.println("MERGE SORT:");
-        for (int n = 0; n < afterMerSort.length; n++) {
-            System.out.print(afterMerSort[n] + ", ");
-        }
-        System.out.println("");
-        
-        System.out.println("++"+checkSort(merSortSampleArr));
-        
-        RunTimes r= new RunTimes();
-        
-        r.getRunTimes(b, arr);
-        
+    }
+
+    public void testSort(String nameOfSort, Sorter s, int[] arrToSort) {
+
+        int[] afterSortArr = s.sort(arrToSort);
+        displayArr(nameOfSort, afterSortArr);
 
     }
 
     public boolean checkSort(int[] arr) {
-        
+
         boolean check = false;
-        
-        for (int i = 0; i < arr.length-1; i++) {
-            if (arr[i] <= arr[i+1]) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] <= arr[i + 1]) {
                 check = true;
-                //System.out.println("true");
-                
             } else {
                 check = false;
-                //System.out.println("false");
                 break;
-                
             }
         }
         return check;
     }
-    
-    public int[] getRanArr(int arrSize){
-        
-        int[] randomArr = new int[arrSize]; 
-        
+
+    public int[] getRanArr(int arrSize) {
+
+        int[] randomArr = new int[arrSize];
+
         Random r = new Random();
-        
-        for(int i = 0;i<arrSize;i++){
-            randomArr[i]=r.nextInt(1000);
+
+        for (int i = 0; i < arrSize; i++) {
+            randomArr[i] = r.nextInt(1000);
         }
-        
         return randomArr;
+    }
+
+    public long displayRunTimes(Sorter s, int[] arrToSort) {
+
+
+        long startTimes = java.time.Instant.now().toEpochMilli();
+        s.sort(arrToSort);
+        long endTimes = java.time.Instant.now().toEpochMilli();
+        long runTimes = (endTimes - startTimes);
+        
+        return runTimes;
+
+    }
+
+    public void displayArr(String nameOfSort, int[] sortedArr) {
+        System.out.println("");
+        System.out.println("--------------------------------------------------");
+        System.out.println(nameOfSort + ": ");
+        for (int n = 0; n < sortedArr.length; n++) {
+            System.out.print(sortedArr[n] + ", ");
+        }
+
     }
 }
